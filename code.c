b9f850ff538d147c9d5c74c2906ce6eedc99800e
zarin <81378664+zarintaufique@users.noreply.github.com>
2022-02-12 22:10:48 +0500

    Completed code

--- code.c
+++ code.c
@@ -10,7 +10,6 @@
 #include <string.h>
 #include <semaphore.h>
 #include <sys/shm.h>
-
 int main(int argc, char* argv[]) { 
 	
 	int size = 0;
@@ -50,6 +49,80 @@ int main(int argc, char* argv[]) {
 	shmdt(s3);
 	shmdt(s4);
 	
-	
+	int pid = fork();
+	if (pid == 0){
+		
+		sem_wait(s1);
+		//filename reading as command line argument
+		char* filename = (char*)malloc (sizeof(char)*40);
+		filename = strdup(argv[1]);
+		int fd = open (filename, O_RDONLY);
+		
+		char* temp = ( char* ) malloc (sizeof (char) * 100);
+		int bytesread = 0, d = 0;
+		 size = read (fd, temp, 100);
+		int* buffer1 = ( int* ) malloc (sizeof (int) * size);	
+		int i = 0;
+			//conversion from char array to int
+		for(;sscanf(temp, "%d%n", &d, &bytesread) > 0; i++)
+		{
+			buffer1[i] = d; //converting to int array
+			temp = temp + bytesread;
+		}
+			close(fd);
+		
+		sem_post(s2);  
+		
+		sem_wait(s3);
+		shmdt(buffer1);
+		
+		sem_wait(s3);
+		int fd1 = open(filename, O_WRONLY);
+		write(fd1, buffer2, size);
+		close(fd1);
+		int j = 0;
+		for( j = 0;j < sizeof(buffer2);j++ ){
+			printf("%d\n", buffer2[j] );
+		}
+		sem_post(s4);
+	}
+	else {
+			sem_wait(s2);
+			int size1 = sizeof(buffer1);
+			int a = 0, k = 0, j = 0, i = 0;
+			int* buffer2 = (int*) malloc (sizeof(int) * size1);
+			for ( i = 0;i < size1 - 1;i++) {
+				for( j = i + 1;j < size1;j++) {
+					if(buffer1[i] == buffer1[j])
+						a++;
+				}
+				//no duplicate
+				if(a == 0){
+					buffer2[k] = buffer1[i];
+					k++;
+				}
+				a = 0;
+			}
+			sem_post(s3);
+			sem_wait(s4);
+
+			//detachment of the pointers
+			shmdt(s1);
+			shmdt(s2);
+			shmdt(buffer1);
+			//deletion of all shared memories
+			shmctl(skey, IPC_RMID, NULL);
+			shmctl(skey1, IPC_RMID, NULL);
+			shmctl(skey2, IPC_RMID, NULL);
+			shmctl(skey3, IPC_RMID, NULL);
+			shmctl(id_cs1, IPC_RMID, NULL);
+			shmctl(id_cs2, IPC_RMID, NULL);
+
+			sem_destroy(s1);
+			sem_destroy(s2);
+			sem_destroy(s3);
+			sem_destroy(s4);
+
+	}
 	return 0;
 }
